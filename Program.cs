5db69fcfe15ae34c6879e8888aa583a13e0bf3a7
adrianofrz <ferraz.adriano3@gmail.com>
2020-10-31 10:38:18 -0300

    Criação do repositório

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace POCVariavelAmbiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            string variavel = null;
+            string valorvariavelAmbiente = null;
+
+            variavel = Console.ReadLine();
+            valorvariavelAmbiente = WindowsEnvarioment.getVariable(variavel);
+            Console.WriteLine("Valor da variavel:   " + valorvariavelAmbiente);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WindowsEnvarioment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCVariavelAmbiente
+{
+    public class WindowsEnvarioment
+    {
+        static string valorvariavelAmbiente = null;        
+
+        public static string getVariable(string nomeVariavel)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            { 
+                valorvariavelAmbiente = Environment.GetEnvironmentVariable(nomeVariavel);               
+            }
+            else
+            {
+                //Console.WriteLine("Ops... deu ruim =(");
+            }
+
+            return valorvariavelAmbiente;
+        }
+    }
+}
